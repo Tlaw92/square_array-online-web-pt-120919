8730c9e59286684cb397906a56e9c9ab96ed0ab7
travis Lawson <travislawclimb@gmail.com>
2019-12-19 20:50:20 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,8 +1,9 @@
-def square_array(array)
-  numbers = [1,2,3]
-  
-end
+#def square_array(array)
+ # numbers = [1,2,3]
+ #  
+#end
 
 #names = Array.new(20)
 #puts names.size  # This returns 20
 #puts names.length # This also returns 20
+
